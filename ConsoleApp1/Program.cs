236c225f53d3f8d842ded746a0a860aeb6b7a00d
cclaurevju <Carlos.Claure@jala.university>
2025-04-29 14:00:27 -0400

    added tests and gitignore

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Figgle;
 
-Console.WriteLine(FiggleFonts.Standard.Render("Hello, World!!"));
+
+namespace MyConsoleApp
+{
+    public class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FiggleFonts.Standard.Render("Hello, World!!"));
+        }
+        public bool IsPositive(int number)
+        {
+            return number > 0;
+        }
+    }
+}
--- TestConsoleApp1/UnitTest1.cs
+++ TestConsoleApp1/UnitTest1.cs
@@ -1,10 +1,20 @@
 namespace TestConsoleApp1;
 
+using System.Security.Cryptography.X509Certificates;
+using MyConsoleApp;
 [TestClass]
 public class UnitTest1
 {
+    private readonly App myApp;
+
+    public UnitTest1()
+    {
+        myApp = new App();
+    }
     [TestMethod]
     public void TestMethod1()
     {
+        bool result = myApp.IsPositive(1);
+        Assert.IsTrue(result, "NOT PASSED!");
     }
 }
